dae64ea2d475025554234a1e21df763365c61e0a
Anthios314 <55056986+Anthios314@users.noreply.github.com>
2020-12-18 17:07:42 -0500

    Balancing the paradox formula... how many times have we done this...
    1/3 --> 1 --> 2/5.
    Lets hope this is the last time we do this crap.

--- javascripts/mods/ngm5.js
+++ javascripts/mods/ngm5.js
@@ -59,7 +59,7 @@ function isDimUnlocked(d) {
 //Paradox Sacrifices
 function getPxGain() {
 	let r = new Decimal(player.matter.max(player.money).max(1).log10()+1)
-	for (var d = 1; d < 9; d++) r = r.times(Math.pow(player[TIER_NAMES[d]+"Amount"].max(10).log10(), 1))
+	for (var d = 1; d < 9; d++) r = r.times(Math.pow(player[TIER_NAMES[d]+"Amount"].max(10).log10(), 2/5)) //we are gonna fiddle with this value a LOT.
 	if (hasPU(44)) r = r.times(puMults[44]())
 	return r.floor()
 }
