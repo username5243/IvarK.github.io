e69ce72a6fc2d6fbeea78251f102686888b508b2
Aarex Tiaokhiao <aarextiaokhiao@gmail.com>
2021-04-15 22:52:09 -0400

    Patch 1

--- javascripts/mods/ngp3/positrons.js
+++ javascripts/mods/ngp3/positrons.js
@@ -3,6 +3,7 @@ function setupPositronSave() {
 		amt: 0,
 		sacBoosts: 0,
 		sacGals: 0,
+		consumedQE: 0,
 		eng: 0,
 		boosts: 0
 	}
@@ -12,13 +13,14 @@ function updatePositronsTab() {
 	ENTANGLED_BOOSTS.updateOnTick("pos")
 
 	let amt = Math.floor(player.meta.resets / 4)
+	getEl("sac_mdb").className = "gluonupgrade " + (amt <= tmp.qu.pos.sacBoosts ? "unavailablebtn" : "storebtn")
 	getEl("sac_mdb").textContent = "Sacrifice " + shortenDimensions(amt - tmp.qu.pos.sacBoosts) + " Meta-Dimension Boosts to gain " + shortenDimensions(getPositronAmt(amt) - tmp.qu.pos.amt) + " Positrons"
 	getEl("positrons_amt").textContent = shortenDimensions(tmp.qu.pos.amt)
 }
 
 function sacrificeMDBs() {
 	let amt = Math.floor(player.meta.resets / 4)
-	if (amt <= tmp.qu.pos.boosts) return
+	if (amt <= tmp.qu.pos.sacBoosts) return
 	tmp.qu.pos.sacBoosts = amt
 	tmp.qu.pos.amt = getPositronAmt(amt)
 
